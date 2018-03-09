d38f69a4ec4863ad4bef4ff23b809a2eaccd7a79
Prescott Breeden <prescott.breeden@gmail.com>
2018-03-09 13:12:57 -0800

    added session and redirect to display submitted information

--- apple.py
+++ apple.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'lakdjf;lakjdf;'
 
 @app.route('/')
 def index():
@@ -11,16 +12,22 @@ def dojos():
 
 @app.route('/dojos/new', methods=['POST'])
 def new_ninja():
-	print(request.form['first_name'])
-	print(request.form['last_name'])
-	print(request.form['email'])
-	print(request.form['password'])
-	print(request.form['gender'])
-	print(request.form['favorite_language'])
-	return redirect('/')
+	session['first_name'] = request.form['first_name']
+	session['last_name'] = request.form['last_name']
+	session['email'] = request.form['email']
+	session['password'] = request.form['password']
+	return redirect('/view_info')
 
 @app.route('/ninjas')
 def ninjas():
 	return render_template("ninjas.html")
+
+@app.route('/view_info')
+def view_info():
+	user_fn = session['first_name']
+	user_ln = session['last_name']
+	user_email = session['email']
+	user_pw = session['password']
+	return render_template("view_info.html", first_name=user_fn, last_name=user_ln, email=user_email, pw=user_pw)
 
 app.run(debug=True)
